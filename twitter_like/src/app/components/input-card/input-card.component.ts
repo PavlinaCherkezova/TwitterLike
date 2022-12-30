0e567a37d105ce5d3bf5a8abcbe80820366ff1dd
pavlina.cherkezova <pavlina.cherkezova@lights.digital>
2022-12-30 16:18:36 +0200

    style: adjust height of card dynamically

--- twitter_like/src/app/components/input-card/input-card.component.ts
+++ twitter_like/src/app/components/input-card/input-card.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { faPersonDotsFromLine } from '@fortawesome/free-solid-svg-icons';
-import { Tweet } from '../../models/tweet';
 import { TweetSubmitService } from '../../services/tweet-submit.service';
 
 
@@ -34,6 +33,31 @@ export class InputCardComponent implements OnInit {
   public onTweetSubmit(): void {
     this.tweetService.addTweet(this.tweetService.generateTweet(this.input));
     this.input = '';
+  }
+
+  public onContentPaste(event: ClipboardEvent): void {
+    const clipboardData = event.clipboardData || (window as any).clipboardData;
+    const items: DataTransferItem[] = Array.from(clipboardData.items);
+    const imageData = items.find(x => /image/i.test(x.type));
+
+    if (imageData) {
+      let blob = imageData.getAsFile();
+      let reader = new FileReader();
+
+      reader.onload = function (e) {
+        if (e.target && e.target.result){
+          let img = new Image();
+          img.src = e.target!.result!.toString();
+          img.width = 128;
+          img.height = 128;
+          img.style.margin = '5px';
+  
+          document.getElementById('images-container')!.appendChild(img);
+        }
+      }
+
+      reader.readAsDataURL(blob as Blob);
+    }
   }
 
 }
